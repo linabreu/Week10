64298a1d46c0c25ca3ee809ef9cc0a10158c25a0
linabreu <linabreu@outlook.com>
2023-08-01 14:41:37 -0700

    added rest of files

--- grocery.js
+++ grocery.js
@@ -1,15 +1,50 @@
-let id = 0;
-button = document.getElementById('addBtn');
 
-/*
-button.addEventListener('click', () =>
-{
-    //test
-    document.getElementById('tableTitle').innerHTML = "My list";
-    //let table = document.getElementById('groceryTable');
-   // let row = table.insertRow(1);
-    //row.insertCell(0).innerHTML = document.getElementById('itemToPurchase').value;
-    //row.insertCell(1).innerHTML = document.getElementById('quantity').value;
-    //row.insertCell(2).innerHTML = document.getElementById('department').value;
+let rowID = 1;
+
+
+//function for populating the value from the date picker
+let date; //undefined and then reassign in the function
+document.getElementById("shoppingDate").addEventListener("change", function() {
+    var input = this.value;
+    var inputAsDate = new Date(input);
+    formattedDate =  `${inputAsDate.getMonth() +1}/${inputAsDate.getDate() + 1}/${inputAsDate.getFullYear()} `;
+    date = formattedDate;
 });
-*/
+
+let createDeleteButton = (rowID) =>
+{
+   let deleteButton = document.createElement('button');
+   deleteButton.className = 'btn btn-warning';
+   deleteButton.id = `buttonNum${rowID}`;
+   deleteButton.innerHTML = "Delete";
+   deleteButton.onClick = () =>
+   {
+        let toDelete = document.getElementById(rowID);
+        let groceryTable = document.getElementById("groceryTable");
+        groceryTable.removeChild();
+        //console.log("works!")
+   }
+   return deleteButton;
+}
+
+//function to be executed when the button is clicked
+let button = document.getElementById('addBtn');
+const  addItem = () =>
+{
+    let store = document.getElementById('store').value;
+    document.getElementById('tableTitle').innerHTML = `${store} List for ${date}`;
+    
+
+    let table = document.getElementById('groceryTable');
+    let row = table.insertRow(1);
+    row.id = rowID
+    row.insertCell(0).innerHTML = document.getElementById('itemToPurchase').value;
+    row.insertCell(1).innerHTML = document.getElementById('quantity').value;
+    row.insertCell(2).innerHTML = document.getElementById('department').value;
+    row.insertCell(3).append(createDeleteButton(rowID));
+
+    rowID++;
+};
+
+button.addEventListener('click', addItem);
+createDeleteButton(1);
